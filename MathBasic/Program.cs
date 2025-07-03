671b83b254d8d36d7c0b66ac49397db88b28b90c
Dilujan Premaranjan <43193341+preamaranjan-dilujan@users.noreply.github.com>
2025-07-03 11:23:48 +0000

    kthDigit

--- MathBasic/Program.cs
+++ MathBasic/Program.cs
@@ -7,7 +7,8 @@ namespace MathBasic
         static void Main(string[] args)
         {
             // CountDigitInANumber(325345);
-            CornerDigitsSum(5);
+            // CornerDigitsSum(5);
+            Console.WriteLine(kthDigit(5, 2, 2)); // Should return 4
 
         }
 
@@ -29,19 +30,37 @@ namespace MathBasic
             int lastDigit = 0, firstDigit = 0;
             while (n > 0)
             {
-            int digit = n % 10;
-            n = n / 10;
-            if (count == 1)
-            {
-                lastDigit = digit;
-            }
-            if (count > 1 && n == 0)
-            {
-                firstDigit = digit;
-            }
-            count++;
+                int digit = n % 10;
+                n = n / 10;
+                if (count == 1)
+                {
+                    lastDigit = digit;
+                }
+                if (count > 1 && n == 0)
+                {
+                    firstDigit = digit;
+                }
+                count++;
             }
             Console.WriteLine(lastDigit + firstDigit);
+        }
+
+        static int kthDigit(int a, int b, int k)
+        {
+            long value = (long)Math.Pow(a, b);
+            int count = 1;
+            int kthDigit = 0;
+            while (value > 0)
+            {
+                int lastDigit = (int)(value % 10);
+                if (count == k)
+                {
+                    kthDigit = lastDigit;
+                }
+                value = value / 10;
+                count++;
+            }
+            return kthDigit;
         }
 
     }
--- MathBasic/obj/Debug/net8.0/MathBasic.AssemblyInfo.cs
+++ MathBasic/obj/Debug/net8.0/MathBasic.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MathBasic")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ecc999b5f23f0184adbae11c38de818b8bb708ff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cbfc715f353e4e9d1ff76724ca2cd2dde77e7777")]
 [assembly: System.Reflection.AssemblyProductAttribute("MathBasic")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MathBasic")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
